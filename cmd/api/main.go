834d8440f2b7f860dd6ba8db88863efebcd79d64
alanloffler <alanmatiasloffler@gmail.com>
2026-05-06 20:57:18 -0300

    feat(api): get env db config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,7 +11,10 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr: env.GetString("DB_ADDR", "postgres://user:password@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://user:password@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
 		},
 	}
 
