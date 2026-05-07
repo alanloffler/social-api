9850954bf3040ab69a36b060373dc37a4420351d
alanloffler <alanmatiasloffler@gmail.com>
2026-05-07 19:38:31 -0300

    fix(store): tags as array

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -62,7 +62,7 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		&post.UserID,
 		&post.Title,
 		&post.Content,
-		&post.Tags,
+		pq.Array(&post.Tags),
 		&post.CreatedAt,
 		&post.UpdatedAt,
 	)
