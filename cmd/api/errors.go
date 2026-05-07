b3ca206046300b0dcc2921478ad6eb4726a7b5e9
alanloffler <alanmatiasloffler@gmail.com>
2026-05-07 19:57:29 -0300

    feat(errors): badRequestResponse

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,7 +6,13 @@ import (
 )
 
 func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
 
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+
+	writeJSONError(w, http.StatusBadRequest, err.Error())
 }
