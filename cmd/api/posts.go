c69ec406d6b60b3056dd17514465a1ac96270c05
alanloffler <alanmatiasloffler@gmail.com>
2026-05-07 20:00:50 -0300

    refactor(posts): notFoundResponse

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -56,7 +56,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
-			writeJSONError(w, http.StatusNotFound, err.Error())
+			app.notFoundResponse(w, r, err)
 		default:
 			app.internalServerError(w, r, err)
 		}
