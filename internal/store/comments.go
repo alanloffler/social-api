3abbec80c3a14c01cae5237b42315f5e47c0d936
alanloffler <alanmatiasloffler@gmail.com>
2026-05-07 21:25:10 -0300

    feat(comments): GetByPostID query

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"context"
 	"database/sql"
 )
 
@@ -10,8 +11,57 @@ type Comment struct {
 	UserID    int64  `json:"user_id"`
 	Content   string `json:"content"`
 	CreatedAt string `json:"created_at"`
+	User      User   `json:"user"`
 }
 
 type CommentStore struct {
 	db *sql.DB
+}
+
+func (s *CommentStore) GetByPostID(ctx context.Context, postID int64) ([]Comment, error) {
+	query := `
+		SELECT
+		  c.id,
+		  c.post_id,
+		  c.user_id,
+		  c.content,
+		  c.created_at,
+		  users.username,
+		  users.id
+		FROM
+		  comments c
+		  JOIN users ON users.id = c.user_id
+		WHERE
+		  c.post_id = $1
+		ORDER BY
+		  c.created_at DESC
+	`
+
+	rows, err := s.db.QueryContext(ctx, query, postID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	comments := []Comment{}
+	for rows.Next() {
+		var c Comment
+		c.User = User{}
+		err := rows.Scan(
+			&c.ID,
+			&c.PostID,
+			&c.UserID,
+			&c.Content,
+			&c.CreatedAt,
+			&c.User.Username,
+			&c.User.ID,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		comments = append(comments, c)
+	}
+
+	return comments, nil
 }
