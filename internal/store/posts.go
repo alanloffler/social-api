3fe2ce9633421f5e537436eec412724d829e4979
alanloffler <alanmatiasloffler@gmail.com>
2026-05-16 20:15:37 -0300

    feat(store): delete post query

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -78,3 +78,24 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 
 	return &post, nil
 }
+
+func (s *PostStore) Delete(ctx context.Context, id int64) error {
+	query := `
+		DELETE FROM posts WHERE id = $1
+	`
+
+	res, err := s.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+}
