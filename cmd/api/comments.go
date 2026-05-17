9c4fff0098830f92a46c7aa869e8a74702325261
alanloffler <alanmatiasloffler@gmail.com>
2026-05-17 16:06:59 -0300

    feat(comments): createCommentHandler

---
+++ cmd/api/comments.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/alanloffler/social/internal/store"
+)
+
+type CreateCommentPayload struct {
+	Content string `json:"content" validate:"required,max=1000"`
+}
+
+func (app *application) createCommentHandler(w http.ResponseWriter, r *http.Request) {
+	post := getPostFromCtx(r)
+
+	var payload CreateCommentPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	comment := &store.Comment{
+		PostID:  post.ID,
+		UserID:  1,
+		Content: payload.Content,
+	}
+
+	if err := app.store.Comments.Create(r.Context(), comment); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusCreated, comment); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
